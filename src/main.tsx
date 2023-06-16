9bdba64da09ac325c22cb1f75a6daee930036933
cdefg666 <791150351@qq.com>
2023-06-16 13:29:01 +0800

    feat: add toolkits page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Context, contextStore } from "./context";
 import "./index.css";
+import Toolkits from "./pages/toolkits";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Context.Provider value={contextStore}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/toolkits" element={<Toolkits />} />
+      </Routes>
+    </BrowserRouter>
   </Context.Provider>
 );
---
+++ src/pages/toolkits/index.tsx
@@ -0,0 +1,388 @@
+import React, { useState, useContext, useEffect, useRef } from "react";
+import { useWallet, useStream } from "../../hooks";
+import { Context } from "../../context";
+import { Model } from "../../types";
+import {
+  PushNotificationClient,
+  PushChatClient,
+  ENV,
+  ModelType as PushModelType,
+  getICAPAddress,
+} from "@dataverse/push-client-toolkit";
+
+import LivepeerClient, {
+  LivepeerConfig,
+} from "@dataverse/livepeer-client-toolkit";
+
+import TablelandClient from "@dataverse/tableland-client-toolkit";
+import { Network } from "@dataverse/tableland-client-toolkit";
+import { LivepeerWidget, LivepeerPlayer } from "../../components/Livepeer";
+
+function Toolkits() {
+  const { output, runtimeConnector } = useContext(Context);
+  const [pushChannelModel, setPushChannelModel] = useState<Model>();
+  const pushChatClientRef = useRef<PushChatClient>();
+  const pushNotificationClientRef = useRef<PushNotificationClient>();
+  const livepeerClientRef = useRef<LivepeerClient>();
+  const tablelandClientRef = useRef<TablelandClient>();
+  const [tableId, setTableId] = useState<string>();
+  const [tableName, setTableName] = useState<string>();
+  const [asset, setAsset] = useState<any>(null);
+
+  const { address, connectWallet, switchNetwork } = useWallet();
+
+  useEffect(() => {
+    (async () => {
+      const name = output.createDapp.name;
+      const slug = output.createDapp.slug;
+
+      const pushChatMessageModel = output.createDapp.streamIDs.find(
+        (item) => item.name === `${slug}_pushchatmessage`
+      );
+
+      const pushChannelModel = output.createDapp.streamIDs.find(
+        (item) => item.name === `${slug}_pushchannel`
+      );
+
+      const pushChatGPGKeyModel = output.createDapp.streamIDs.find(
+        (item) => item.name === `${slug}_pushchatgpgkey`
+      );
+
+      const pushNotificationModel = output.createDapp.streamIDs.find(
+        (item) => item.name === `${slug}_pushnotification`
+      );
+
+      const livepeerModel = output.createDapp.streamIDs.find(
+        (item) => item.name === `${slug}_livepeerasset`
+      );
+
+      const tablelandModel = output.createDapp.streamIDs.find(
+        (item) => item.name === `${slug}_table`
+      );
+
+      if (pushChatMessageModel) {
+        const pushChatClient = new PushChatClient({
+          runtimeConnector,
+          modelIds: {
+            [PushModelType.MESSAGE]: pushChatMessageModel?.stream_id!,
+            [PushModelType.USER_PGP_KEY]: pushChatGPGKeyModel?.stream_id!,
+            [PushModelType.CHANNEL]: pushChannelModel?.stream_id!,
+            [PushModelType.NOTIFICATION]: pushNotificationModel?.stream_id!,
+          },
+          appName: output.createDapp.name,
+          env: ENV.STAGING,
+        });
+        pushChatClientRef.current = pushChatClient;
+      }
+
+      if (pushNotificationModel) {
+        const pushNotificationClient = new PushNotificationClient({
+          runtimeConnector,
+          modelIds: {
+            [PushModelType.MESSAGE]: pushChatMessageModel?.stream_id!,
+            [PushModelType.USER_PGP_KEY]: pushChatGPGKeyModel?.stream_id!,
+            [PushModelType.CHANNEL]: pushChannelModel?.stream_id!,
+            [PushModelType.NOTIFICATION]: pushNotificationModel?.stream_id!,
+          },
+          appName: output.createDapp.name,
+          env: ENV.STAGING,
+        });
+        pushNotificationClientRef.current = pushNotificationClient;
+      }
+
+      if (tablelandModel) {
+        const tablelandClient = new TablelandClient({
+          runtimeConnector,
+          network: Network.MUMBAI,
+          modelId: tablelandModel?.stream_id,
+        });
+        tablelandClientRef.current = tablelandClient;
+      }
+
+      if (livepeerModel) {
+        const livepeerClient = new LivepeerClient({
+          apiKey: "6bbdad77-25ed-42b9-9b6d-b419766410f7",
+          runtimeConnector,
+          modelId: livepeerModel.stream_id,
+          appName: name,
+        });
+        livepeerClientRef.current = livepeerClient;
+      }
+
+      if (pushChannelModel) {
+        setPushChannelModel(pushChannelModel);
+      }
+    })();
+  }, []);
+
+  const { pkh, createCapability } = useStream();
+
+  const connect = async () => {
+    const { wallet } = await connectWallet();
+    const pkh = await createCapability(wallet);
+    console.log("pkh:", pkh);
+    return pkh;
+  };
+
+  // Push Notifications
+  const getUserSubscriptions = async () => {
+    if (!address) {
+      throw new Error("address undefined");
+    }
+    const subscriptions =
+      await pushNotificationClientRef.current?.getSubscriptionsByUser(address);
+    console.log("[getUserSubscriptions]subscriptions:", subscriptions);
+  };
+
+  const getUserSpamNotifications = async () => {
+    if (!address) {
+      throw new Error("address undefined");
+    }
+    const spams =
+      await pushNotificationClientRef.current?.getUserSpamNotifications(
+        getICAPAddress(address)
+      );
+    console.log("[getUserSpamNotifications]notifications:", spams);
+  };
+
+  const getNotificationsByUser = async () => {
+    const notifications =
+      await pushNotificationClientRef.current?.getNotificationsByUser(
+        address!,
+        1,
+        100
+      );
+    console.log("[getNotificationsByUser]notifications:", notifications);
+  };
+
+  const subscribe = async () => {
+    const subscribeChannel =
+      "eip155:5:0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+    try {
+      await pushNotificationClientRef.current?.subscribeChannel(
+        subscribeChannel
+      );
+      console.log("[subscribe]done");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const unsubscribe = async () => {
+    const subscribeChannel =
+      "eip155:5:0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+    try {
+      await pushNotificationClientRef.current?.unsubscribeChannel(
+        subscribeChannel
+      );
+      console.log("[unsubscribe]done");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const sendNotification = async () => {
+    const sendChannel = "eip155:5:0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+    const title = "Hello Title";
+    const body = "Tom long time no see.";
+    const img =
+      "https://bafkreicf7ynxjjjumhrntswhtqbnkjvi24zqw3ubuvbeie3nzrylyyovye.ipfs.dweb.link";
+    const cta = "";
+
+    try {
+      const res = await pushNotificationClientRef.current?.sendNotification(
+        sendChannel,
+        title,
+        body,
+        img,
+        cta
+      );
+      console.log("[sendNotification]res:", res);
+    } catch (error: any) {
+      if (error.message === "this account does not have channel") {
+        console.warn(
+          "This Account doesn't have channel, please go to https://staging.push.org/ to create channel."
+        );
+      }
+    }
+  };
+
+  const getChannelDetail = async () => {
+    const detailChannel = "eip155:5:0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+    const channelData =
+      await pushNotificationClientRef.current?.getChannelDetail(detailChannel);
+    console.log("[getChannelDetail]channelData:", channelData);
+  };
+
+  const getSubscriberOfChannel = async () => {
+    const queryChannel = "eip155:5:0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+    const page = 1;
+    const limit = 10;
+    const subscribers =
+      await pushNotificationClientRef.current?.getSubscriberOfChannel(
+        queryChannel,
+        page,
+        limit
+      );
+    console.log("[getSubscriberOfChannel]subscribers:", subscribers);
+  };
+
+  const searchChannelByName = async () => {
+    const searchName = "DataverseChannel";
+    const channelsData =
+      await pushNotificationClientRef.current?.searchChannelByName(
+        searchName,
+        1,
+        10
+      );
+    console.log("[searchChannelByName]channelsData:", channelsData);
+  };
+
+  const getNotificationList = async () => {
+    const noticeList =
+      await pushNotificationClientRef.current?.getNotificationList();
+    console.log("getNotificationList: ", noticeList);
+  };
+
+  // Push Chat
+  const createPushChatUser = async () => {
+    const user = await pushChatClientRef.current?.createPushChatUser();
+    console.log("CreatePushChatUser: response: ", user);
+  };
+
+  const sendChatMessage = async () => {
+    const msgCont = "Someting content";
+    const msgType = "Text";
+    const receiver = "0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+
+    const response = await pushChatClientRef.current?.sendChatMessage(
+      receiver,
+      msgCont,
+      msgType
+    );
+
+    console.log("sendChatMessage: response: ", response);
+  };
+
+  const fetchHistoryChats = async () => {
+    const receiver = "0xcbeE6DdA2347C0EC0e45870d4D6cf3526a2E319C";
+    const limit = 30;
+
+    const response = await pushChatClientRef.current?.fetchHistoryChats(
+      receiver,
+      limit
+    );
+
+    console.log("FetchHistoryChats: response: ", response);
+  };
+
+  const getChatMessageList = async () => {
+    const msgList = await pushChatClientRef.current?.getMsgList();
+    console.log("ChatMessageList: response: ", msgList);
+  };
+
+  // Tableland
+  const createTable = async () => {
+    await switchNetwork(80001);
+
+    const CREATE_TABLE_SQL =
+      "CREATE TABLE test_table (id integer primary key, record text)";
+    console.log(tablelandClientRef.current);
+    const res = await tablelandClientRef.current?.createTable(CREATE_TABLE_SQL);
+    setTableId(res?.tableId);
+    setTableName(`${res?.tableName}_${res?.chainId}_${res?.tableId}`);
+    console.log("CreateTable: response: ", res);
+  };
+
+  const insertTable = async () => {
+    const MUTATE_TABLE_SQL = `INSERT INTO ${tableName} (id, record) values(1, 'hello man01')`;
+
+    const res = await tablelandClientRef.current?.mutateTable(
+      tableId!,
+      MUTATE_TABLE_SQL
+    );
+    console.log("InsertTable: response: ", res);
+  };
+
+  const updateTable = async () => {
+    const UPDATE_TABLE_SQL = `UPDATE ${tableName} SET record = 'hello man02' WHERE id = 1`;
+
+    const res = await tablelandClientRef.current?.mutateTable(
+      tableId!,
+      UPDATE_TABLE_SQL
+    );
+    console.log("UpdateTable: response: ", res);
+  };
+
+  const getTableByTableId = async () => {
+    const tablelandClient = tablelandClientRef.current;
+    const tableName = await tablelandClient?.getTableNameById(tableId!);
+    if (tableName) {
+      const result = await tablelandClient?.getTableByName(tableName);
+      console.log("GetTableByTableId:", result);
+    } else {
+      console.error("getTableNameById failed");
+    }
+  };
+
+  const getTableList = async () => {
+    const tablelandClient = tablelandClientRef.current;
+    const tables = await tablelandClient?.getTableList();
+    console.log("tables: ", tables);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={connect}>connect</button>
+      <div className="blackText">{pkh}</div>
+      <hr />
+      <h2 className="label">Push Channel</h2>
+      <button onClick={getUserSubscriptions}>getUserSubscriptions</button>
+      <button onClick={getUserSpamNotifications}>
+        getUserSpamNotifications
+      </button>
+      <button onClick={getNotificationsByUser}>getNotificationsByUser</button>
+      <button onClick={subscribe}>subscribe</button>
+      <button onClick={unsubscribe}>unsubscribe</button>
+      <button onClick={sendNotification}>sendNotification</button>
+      <button onClick={getChannelDetail}>getChannelDetail</button>
+      <button onClick={getSubscriberOfChannel}>getSubscriberOfChannel</button>
+      <button onClick={searchChannelByName}>searchChannelByName</button>
+      <button onClick={getNotificationList}>getNotificationList</button>
+      <br />
+      <h2 className="label">Push Chat</h2>
+      <button onClick={createPushChatUser}>createPushChatUser</button>
+      <button onClick={sendChatMessage}>sendChatMessage</button>
+      <button onClick={fetchHistoryChats}>fetchHistoryChats</button>
+      <button onClick={getChatMessageList}>getChatMessageList</button>
+      <br />
+      <h2 className="label">Tableland</h2>
+      <button onClick={createTable}>createTable</button>
+      <button onClick={insertTable}>insertTable</button>
+      <button onClick={updateTable}>updateTable</button>
+      <button onClick={getTableByTableId}>getTableByTableId</button>
+      <button onClick={getTableList}>getTableList</button>
+      <br />
+      <h2 className="label">Livepeer</h2>
+      {livepeerClientRef.current?.reactClient && (
+        <>
+          <LivepeerConfig client={livepeerClientRef.current.reactClient!}>
+            <LivepeerWidget
+              livepeerClient={livepeerClientRef.current}
+              asset={asset}
+              setAsset={setAsset}
+            />
+            {asset?.id && (
+              <LivepeerPlayer
+                reactClient={livepeerClientRef.current.reactClient}
+                playbackId={asset.playbackId}
+              />
+            )}
+          </LivepeerConfig>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Toolkits;
